75d70c513cc84daba1f8dc12e9a111020350d760
Noah Heinrich <noahheinrich@gmail.com>
2016-03-31 16:17:40 -0500

    Add some rspec

--- spec/overleap_spec.rb
+++ spec/overleap_spec.rb
@@ -1,18 +1,44 @@
 require_relative 'spec_helper'
-hash = { propensity: 1, ranking: 'C' }
 
-js = JSON.generate(hash)
-STUBS = Faraday::Adapter::Test::Stubs.new do |stub|
-  stub.get('/customer_scoring') { |env| [200, {}, js] }
-end
-TEST = Faraday.new do |builder|
-  builder.adapter :test, STUBS do |stub|
-  end
-end
+
 
 describe Overleap do
+  before(:all) do
+    hash = { propensity: 1, ranking: 'C' }
+    js = JSON.generate(hash)
+    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
+      stub.get('/customer_scoring') { |env| [200, {}, js] }
+    end
+
+    @test = Faraday.new do |builder|
+      builder.adapter :test, stubs do |stub|
+      end
+    end
+
+    @data = { income: 20000, zipcode: 60641, age: 25 }
+  end
+
   it 'has a version number' do
     expect(Overleap::VERSION).not_to be nil
+  end
+
+  describe Overleap::Report do
+    describe "#generate_report" do
+      it "creates a Report given a faraday source and data" do
+        response = Overleap::Report.generate_report(@test, @data)
+        expect(response).to be_a Overleap::Report
+      end
+
+      it "generates a report with a propensity and ranking" do
+        response = Overleap::Report.generate_report(@test, @data)
+        expect(response.propensity).to eq 1
+        expect(response.ranking).to eq "C"
+      end
+
+      it "raises a TypeError if not given a Faraday connection" do
+        expect{ Overleap::Report.generate_report(5, @data) }.to raise_error(TypeError)
+      end
+    end
   end
 
 end
