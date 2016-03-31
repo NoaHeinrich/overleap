be83324069d40ae5af64669ad99e34a7d61764f9
Noah Heinrich <noahheinrich@gmail.com>
2016-03-31 14:53:12 -0500

    Figured out stubbing with Faraday

--- spec/overleap_spec.rb
+++ spec/overleap_spec.rb
@@ -1,11 +1,18 @@
-require 'spec_helper'
+require_relative 'spec_helper'
+hash = { propensity: 1, ranking: 'C' }
+
+js = JSON.generate(hash)
+STUBS = Faraday::Adapter::Test::Stubs.new do |stub|
+  stub.get('/customer_scoring') { |env| [200, {}, js] }
+end
+TEST = Faraday.new do |builder|
+  builder.adapter :test, STUBS do |stub|
+  end
+end
 
 describe Overleap do
   it 'has a version number' do
     expect(Overleap::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
