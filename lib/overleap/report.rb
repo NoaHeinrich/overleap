823ac8856a272d8ebb35868a221c3e29af54e749
Noah Heinrich <noahheinrich@gmail.com>
2016-04-01 12:04:28 -0500

    Add rspec testing for error handling

--- lib/overleap/report.rb
+++ lib/overleap/report.rb
@@ -1,18 +1,12 @@
 require 'faraday'
 require 'json'
 
-# FARA = Faraday.new(:url => "http://jsonplaceholder.typicode.com") do |faraday|
-#   faraday.request  :url_encoded
-#   faraday.response :logger
-#   faraday.adapter  Faraday.default_adapter
-# end
-
-
 module Overleap
   class Report
     attr_reader :propensity, :ranking
     def initialize(attributes)
       raise ArgumentError unless attributes.has_key?("propensity") && attributes.has_key?("ranking")
+
       @propensity = attributes["propensity"]
       @ranking = attributes["ranking"]
     end
@@ -22,10 +16,22 @@ module Overleap
       check_data(data)
       response = source.get "/customer_scoring", { :income => data[:income], :zipcode => data[:zipcode], :age => data[:age] }
       attributes = JSON.parse(response.body)
+      check_response(attributes)
       new(attributes)
     end
 
+    def self.create_connection(url)
+      connection = Faraday.new(:url => url) do |faraday|
+        faraday.request  :url_encoded
+        faraday.response :logger
+        faraday.adapter  Faraday.default_adapter
+      end
+      connection.get
+      connection
+    end
+
     private
+
     def self.check_faraday(source)
       raise TypeError, "Use a Faraday Connection" unless source.class == Faraday::Connection
     end
@@ -34,5 +40,8 @@ module Overleap
       raise RuntimeError, "The information you entered was either incomplete or incorrect" unless data.has_key?(:income) && data.has_key?(:zipcode) && data.has_key?(:age)
     end
 
+    def self.check_response(response)
+      raise RuntimeError, "The response did not include correct data" unless response.has_key?("propensity") && response.has_key?("ranking")
+    end
   end
 end
