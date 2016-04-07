53ba32ba9341e03a3f0e9640bf31f22751ad6ffd
Noah Heinrich <noahheinrich@gmail.com>
2016-04-07 16:00:13 -0500

    Adding Report class methods to Overleap module instead

--- lib/overleap/report.rb
+++ lib/overleap/report.rb
@@ -11,40 +11,40 @@ module Overleap
     end
 
 
-    def self.generate_report(source, data)
-      check_faraday(source)
-      check_data(data)
-      response = source.get "/customer_scoring",
-        { :income => data[:income],
-        :zipcode => data[:zipcode],
-        :age => data[:age] }
-      attributes = JSON.parse(response.body)
-      check_response(attributes)
-      new(attributes)
-    end
+    # def self.generate_report(source, data)
+    #   check_faraday(source)
+    #   check_data(data)
+    #   response = source.get "/customer_scoring",
+    #     { :income => data[:income],
+    #     :zipcode => data[:zipcode],
+    #     :age => data[:age] }
+    #   attributes = JSON.parse(response.body)
+    #   check_response(attributes)
+    #   new(attributes)
+    # end
 
-    def self.create_connection(url)
-      connection = Faraday.new(:url => url) do |faraday|
-        faraday.request  :url_encoded
-        faraday.response :logger
-        faraday.adapter  Faraday.default_adapter
-      end
-      connection.get
-      connection
-    end
+    # def self.create_connection(url)
+    #   connection = Faraday.new(:url => url) do |faraday|
+    #     faraday.request  :url_encoded
+    #     faraday.response :logger
+    #     faraday.adapter  Faraday.default_adapter
+    #   end
+    #   connection.get
+    #   connection
+    # end
 
-    private
+    # private
 
-    def self.check_faraday(source)
-      raise TypeError, "Use a Faraday Connection" unless source.class == Faraday::Connection
-    end
+    # def self.check_faraday(source)
+    #   raise TypeError, "Connection not valid. Please use create_connection with a valid url to create a Faraday connection." unless source.is_a? Faraday::Connection
+    # end
 
-    def self.check_data(data)
-      raise RuntimeError, "The information you entered was either incomplete or incorrect" unless data.has_key?(:income) && data.has_key?(:zipcode) && data.has_key?(:age)
-    end
+    # def self.check_data(data)
+    #   raise RuntimeError, "The information you entered was either incomplete or incorrect. Please include income, zipcode, and age." unless data.has_key?(:income) && data.has_key?(:zipcode) && data.has_key?(:age)
+    # end
 
-    def self.check_response(response)
-      raise RuntimeError, "The response did not include correct data" unless response.has_key?("propensity") && response.has_key?("ranking")
-    end
+    # def self.check_response(response)
+    #   raise RuntimeError, "The received response did not include correct data. Please check that your source leads to the correct API." unless response.has_key?("propensity") && response.has_key?("ranking")
+    # end
   end
 end
